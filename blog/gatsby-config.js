aefebd5b0636cf859c658551d7609da0b422694c
saroo2a <murrye2@gmatc.matc.edu>
2023-10-04 22:10:38 -0500

    first commit for activity 2

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -19,30 +19,24 @@ module.exports = {
       address: '9572 west prairie grass way'
     }
   },
-  plugins: [
-    `gatsby-plugin-image`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+    plugins: [
+      'gatsby-plugin-image',
+      {
+        resolve: 'gatsby-source-filesystem',
+        options: {
+          name: 'images',
+          path: `${__dirname}/src/images`,
+        },
       },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      {
+        resolve: 'gatsby-source-contentful',
+        options: {
+          spaceId: 'YOUR SPACE ID',
+          accessToken: 'YOUR ACCESS TOKEN',
+        },
       },
-    },
-  ],
-}
+      'gatsby-transformer-sharp',
+      'gatsby-plugin-sharp',
+    ],
+  };
+  
